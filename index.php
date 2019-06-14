d0a0d0c41dfefc93adbf8c429dc4f2b48512341e
Idanord <33676867+Idanord@users.noreply.github.com>
2019-06-14 09:39:56 -0700

    Temp File
    This commit adds a temp directory that incases files that can me modified for future use with the contact form. This as well has updated the index.php to have lorem ipsum as a place holder until content is given. Added styles.css for all css other than the nav.css.

--- index.php
+++ index.php
@@ -1,64 +1,63 @@
 <?php include 'includes/header.php'?>
     <article id="gamingPage">
         <section class="whatIs">
-            <h2>What Is Legacy Gaming</h2>
-            <p class="premium">We are a premium social gaming company focused on uniting like-minded players of all ages
-                and to creating strong community-based relationships. We believe in mentoring as well as tutoring our
-                youth, hosting internships for our young adults, as well as hardcore gaming and tournament play for
-                all of our patrons.</p>
-            <div class="videoGaming">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/qYfNzhLXYGc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
+            <h2>What is Legacy Gaming</h2>
+            <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
+            <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
+            <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
+            <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
         </section>
-        <section class="features">
-            <h3>Key Features</h3>
-            <div class="featurePoints">
-                <ul>
-                    <li>PC Gaming using our Alienware Area 51 Threadripper  Overclocked PC&apos;s for For high powered
-                        competitive Gaming.</li>
-                    <li>VR Gaming on the HTC VIVE, PS4 VR, and Xbox One X VR in Stand alone and Family room settings.</li>
-                    <li>Console Gaming on Xbox One VR, PS4 Pro, and Nintendo Switch.</li>
-                    <li>Turn Back Time Zone allowing game play on as well as the complete History on
-                        Major Gaming Consoles from the beginning of console gaming starting with the Magnavox Odyssey.</li>
-                    <li>Kids Tutoring in Math using Gaming as an incentive in our All Kids Zone!</li>
-                    <li>Tournament Hosting for Members on many of the most popular games both in-house and remote for
-                        a slightly higher fee.</li>
-                    <li>The ability to socialize and build your own leagues compete amongst each other and upon voting
-                        on the best of the best. Legacy will sponsor them in the largest tournament world-wide pending
-                        qualification (see rules).</li>
-                </ul>
-            </div>
-        </section>
-        <section class="legacyGaming">
-            <h2>Legacy Gaming Offers Power!!</h2>
-            <div class="a51">
-                <img src="images/A51.png" alt="a51"/>
-            </div>
-            <p class="subA51">Legacy Gaming offers the finest virtual reality experience. Check out the reservation and come play!</p>
-            <div class="greatGame">
-                <img src="images/Great-Game-shot-middle.jpg" alt="Great Game shot"/>
-            </div>
-            <p class="subGreatGame">Legacy Gaming offers the finest virtual reality experience. Check out the reservation and come play!</p>
-            <div class="area51">
-                <img src="images/Area51Thrippers.png" alt="Area51 Thrippers"/>
-            </div>
-            <p class="subArea51">Legacy Gaming offers the finest virtual reality experience. Check out the reservation and come play!</p>
-        </section>
-        <section class="gameOn">
-            <div class="videoVR">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/jyDWBkKlYBM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
-        </section>
-        
     </article>
-        <!-- Ask about this content
-        <section class="updates">
-            <div class="leftImg">
-                <img src="#"/>
-            </div>
-            <p class="leftText">Clever stuff goes here</p>
+    <article class="reservations">
+        <section class="form">
+            <p class="formInformation">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
+            <p class="formInformation">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
+            <p class="formInformation">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.</p>
         </section>
-        -->
+    </article>
+
+            <!--Ask for content to go in this spot-->
+            <!--<div class="videoGaming">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/qYfNzhLXYGc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>-->
+            <
+
+            <!-- Ask about this content
+            <section class="updates">
+                <div class="leftImg">
+                    <img src="#"/>
+                </div>
+                <p class="leftText">Clever stuff goes here</p>
+            </section>
+            -->
         
 
 <?php include 'includes/footer.php'?>
