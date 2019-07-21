e863b0208166c1413187313f527feaba291b0b71
Idanord <33676867+Idanord@users.noreply.github.com>
2019-07-21 10:28:17 -0700

    Contact Form
    This updates to where the contact form sends as well as adds in the contact_me.min.js and contact_me.js to the folder.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'support@ravensbard.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'support@legacygamingco.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Legacy Gaming Contact Form:  $name";
 $email_body = "You have received a new message from your Legacy Gaming contact form.\n\n".
     "Here are the details:\n\nName: $name\nEmail: $email_address\nPhone: $phone\nMessage:\n$message";
