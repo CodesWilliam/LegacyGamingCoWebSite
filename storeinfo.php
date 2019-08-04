0a53af776f02d5c16f846d4942ad674e6645d63e
Idanord <33676867+Idanord@users.noreply.github.com>
2019-08-04 10:53:26 -0700

    index and storeinfo updates
    Added titles to each of the sections for the form on the home page. Added a new addtion to the reservations form on the home page for Time. Took out the placeholder text for the phases and added in title of h3 for the phases. Started adding in the phase one text.

--- storeinfo.php
+++ storeinfo.php
@@ -71,11 +71,17 @@
         </section>
         <section class="phases" name="phases">
             <h2>Road Map</h2>
-            <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Phase 1</h3>
+            <ul style="list-style-type:circle;">
+                <li>(10) HTC Vive Consoles</li>
+                <li>(5) PS4 Pro VR</li>
+                <li>Build community based relationships</li>
+                <li>Build business relationships</li>
+                <li>Advertise all Phases to come (to promote growth and backing)</li>
+                <li>See how to incorporate Semnox Card system or other system</li>
+                <li>Begin marketing</li>
+                <li>Work on purchasing all Old Gaming systems for the Turn Back Time zone</li>
+            </ul>
             <p class="premium">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
