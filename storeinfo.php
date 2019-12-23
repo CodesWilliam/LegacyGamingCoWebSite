4d2ae198728145cbd6cfa52c7fafaeb2f6adc41b
Idanord <33676867+Idanord@users.noreply.github.com>
2019-12-23 02:07:15 -0800

    Styles adn StoreInfo
    Styles updated to include the background changed to black  and added a 80% on the body. StoreInfo updated to start the flex box set up.

--- storeinfo.php
+++ storeinfo.php
@@ -72,63 +72,71 @@
         <section class="phases" name="phases">
             <h2>Road Map</h2>
             <div class="phaseInformation">
-                <h3 class="phaseTitle">Phase 1</h3 class="phaseTitle">
-                <ul style="list-style-type:circle;">
-                    <li>(10) HTC Vive Consoles</li>
-                    <li>(5) PS4 Pro VR</li>
-                    <li>Build community based relationships</li>
-                    <li>Build business relationships</li>
-                    <li>Advertise all Phases to come (to promote growth and backing)</li>
-                    <li>See how to incorporate Semnox Card system or other system</li>
-                    <li>Begin marketing</li>
-                    <li>Work on purchasing all Old Gaming systems for the Turn Back Time zone</li>
-                </ul>
-                <h3 class="phaseTitle">Phase 2</h3>
-                <ul style="list-style-type:circle;">
-                    <li>Add (10) PS4 Pro Consoles</li>
-                    <li>Add (10) Xbox One S Consoles</li>
-                    <li>Add (10) Alienware Area 51 PC's</li>
-                    <li>Build community based relationships</li>
-                    <li>Build business relationships</li>
-                    <li>Advertise all phases to come within the marketing strategy
-                    (to promote growth and backing)</li>
-                    <li>Begin progressive work on tutoring and mentoring</li>
-                    <li>Incorporate the food court</li>
-                    <li>Incorporate Semnox or other service (evolving over time)</li>
-                    <li>Start working towards game designer booths</li>
-                    <li>Continue working on purchases for the Turn Back Time zone</li>
-                </ul>
-                <h3 class="phaseTitle">Phase 3</h3>
-                <ul style="list-style-type:circle;">
-                    <li>Add (20) Alienware Area 51 PC's</li>
-                    <li>Add (10) PS4 Pro consoles</li>
-                    <li>Add (5) PS4 Pro VR consoles</li>
-                    <li>Add (10) Xbox One S consoles</li>
-                    <li>Add (10 to 20) Nintendo Switch consoles</li>
-                    <li>Continue to build community based relationships</li>
-                    <li>Begin forming tournament space, rules, and setup</li>
-                    <li>Continue to build business relationships and backing</li>
-                    <li>Grow food court</li>
-                    <li>Begin to prepare for alcohol sales</li>
-                    <li>Advertise all phases to come (to promote growth and backing)</li>
-                    <li>Maintain progressive work on tutoring and mentoring</li>
-                    <li>Begin building the space for our students as well as visits from the school</li>
-                    <li>Continue to work on space for game designers to setup</li>
-                </ul>
-                <h3 class="phaseTitle">Phase 4</h3>
-                <ul style="list-style-type:circle;">
-                    <li>Add (10) HTC Vive consoles family room setting</li>
-                    <li>Ad (5) PS4 Pro VR consoles family room settings</li>
-                    <li>Add (20) PS4 Pro consoles</li>
-                    <li>Add (15) Alienware 51 consoles</li>
-                    <li>Begin full tutoring and memberships</li>
-                    <li>Incorporate lager food court with upgraded menus</li>
-                    <li>Incorporate alcohol</li>
-                    <li>Start field trips for the youth</li>
-                    <li>Grow Semnox, advertising, revamp the website to reflect all changes</li>
-                    <li>Open the Turn Back Time zone</li>
-                    <li>Grow VS Systems by adding more sophisticated VS gaming systems</li>
-                </ul>
+                <div class="row">
+                        <div class="column">
+                            <h3 class="phaseTitle">Phase 1</h3 class="phaseTitle">
+                            <ul style="list-style-type:circle;">
+                                <li>(10) HTC Vive Consoles</li>
+                                <li>(5) PS4 Pro VR</li>
+                                <li>Build community based relationships</li>
+                                <li>Build business relationships</li>
+                                <li>Advertise all Phases to come (to promote growth and backing)</li>
+                                <li>See how to incorporate Semnox Card system or other system</li>
+                                <li>Begin marketing</li>
+                                <li>Work on purchasing all Old Gaming systems for the Turn Back Time zone</li>
+                            </ul>
+                            <h3 class="phaseTitle">Phase 2</h3>
+                            <ul style="list-style-type:circle;">
+                                <li>Add (10) PS4 Pro Consoles</li>
+                                <li>Add (10) Xbox One S Consoles</li>
+                                <li>Add (10) Alienware Area 51 PC's</li>
+                                <li>Build community based relationships</li>
+                                <li>Build business relationships</li>
+                                <li>Advertise all phases to come within the marketing strategy
+                                (to promote growth and backing)</li>
+                                <li>Begin progressive work on tutoring and mentoring</li>
+                                <li>Incorporate the food court</li>
+                                <li>Incorporate Semnox or other service (evolving over time)</li>
+                                <li>Start working towards game designer booths</li>
+                                <li>Continue working on purchases for the Turn Back Time zone</li>
+                            </ul>
+                        </div>
+                </div>
+                <div class="row">
+                    <div class="column">
+                        <h3 class="phaseTitle">Phase 3</h3>
+                        <ul style="list-style-type:circle;">
+                            <li>Add (20) Alienware Area 51 PC's</li>
+                            <li>Add (10) PS4 Pro consoles</li>
+                            <li>Add (5) PS4 Pro VR consoles</li>
+                            <li>Add (10) Xbox One S consoles</li>
+                            <li>Add (10 to 20) Nintendo Switch consoles</li>
+                            <li>Continue to build community based relationships</li>
+                            <li>Begin forming tournament space, rules, and setup</li>
+                            <li>Continue to build business relationships and backing</li>
+                            <li>Grow food court</li>
+                            <li>Begin to prepare for alcohol sales</li>
+                            <li>Advertise all phases to come (to promote growth and backing)</li>
+                            <li>Maintain progressive work on tutoring and mentoring</li>
+                            <li>Begin building the space for our students as well as visits from the school</li>
+                            <li>Continue to work on space for game designers to setup</li>
+                        </ul>
+                        <h3 class="phaseTitle">Phase 4</h3>
+                        <ul style="list-style-type:circle;">
+                            <li>Add (10) HTC Vive consoles family room setting</li>
+                            <li>Ad (5) PS4 Pro VR consoles family room settings</li>
+                            <li>Add (20) PS4 Pro consoles</li>
+                            <li>Add (15) Alienware 51 consoles</li>
+                            <li>Begin full tutoring and memberships</li>
+                            <li>Incorporate lager food court with upgraded menus</li>
+                            <li>Incorporate alcohol</li>
+                            <li>Start field trips for the youth</li>
+                            <li>Grow Semnox, advertising, revamp the website to reflect all changes</li>
+                            <li>Open the Turn Back Time zone</li>
+                            <li>Grow VS Systems by adding more sophisticated VS gaming systems</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </section>
     </article>
