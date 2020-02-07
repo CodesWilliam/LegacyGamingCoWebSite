4df555ad99d0fd5b57cb5f17574893336433f0c2
Idanord <33676867+Idanord@users.noreply.github.com>
2020-02-06 16:50:18 -0800

    Sections and Css Updates
    Updates to the sections on the home page and css updates as needed.

--- index.php
+++ index.php
@@ -15,6 +15,36 @@
                 officia deserunt mollit anim id est laborum.
             </p>
     </section>
+    <section class="storeInfo">
+        <div class="legacyHistory">
+            <h2>Legacy History</h2>
+            <p class="history">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="kidsZoneInfo">
+            <h2>Kids Zone</h2>
+            <p class="kidsZoneText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="retro">
+            <h2>Turn Back Time</h2>
+            <p class="turnBackTime">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </section>
+    <section class="contact">
+        <!--place contact form here-->
+    </section>
+
         
 
 <?php include 'includes/footer.php'?>
