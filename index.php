bf3e23ded7af92d33a7feede883e96a7be58280e
Idanord <33676867+Idanord@users.noreply.github.com>
2020-02-08 14:05:33 -0800

    Content Added
    Wording on the home page added. Added pages to link to with the read more links.

--- index.php
+++ index.php
@@ -8,37 +8,40 @@
                 <img src="../images/smallLogo.png" alt="Legacy Logo">
             </div>
             <p class="legacyOverview">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia deserunt mollit anim id est laborum.
+                Legacy Gaming is a new age Arcade or what we like to call a Gaming
+                Lounge built for all ages of Gamers. We specialize in VR, PC, and all
+                Console Gaming with games of all Genres and no limits. <a href="whatIs.php">(Read More...)</a>
+
             </p>
     </section>
     <section class="storeInfo">
         <div class="legacyHistory">
-            <h2>Legacy History</h2>
-            <p class="history">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia deserunt mollit anim id est laborum.</p>
+            <h2>What Legacy Gaming Offers</h2>
+            <p class="history">
+                We host the best gaming experience a person can have with the fastest
+                speeds and best graphics one can experience. With our environment being
+                hardwired and using Fiber, we host Gigabit speeds that are faster than
+                residential gamers. <a href="offering.php">(Read More...)</a>
+            </p>
         </div>
         <div class="kidsZoneInfo">
-            <h2>Kids Zone</h2>
-            <p class="kidsZoneText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia deserunt mollit anim id est laborum.</p>
+            <h2>Gaming at its finest</h2>
+            <p class="kidsZoneText">
+                At Legacy we have a wide variety of Gaming Systems such as HTC Vive, PS4
+                Pro VR, PS4 Pro (soon to be PS 5), X Box One X, and Nintendo Switch. We
+                also host the fastest in PC Gaming with the Alienware Aurora's R8 for PC
+                Gaming and R9 for our VR experience Overclocked. <a href="finest.php">(Read More...)</a>
+            </p>
         </div>
         <div class="retro">
             <h2>Turn Back Time</h2>
-            <p class="turnBackTime">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                officia deserunt mollit anim id est laborum.</p>
+            <p class="turnBackTime">
+                At Legacy Gaming we not only host what is new and hot but we pay tribute
+                to the gaming consoles that got us here. You can play most of the old
+                consoles as we rotate them in and out. Everything from Pong to Atari and
+                space Invaders to Coleco Vision. Come Game with us and learn a bit of
+                history! <a href="turnBackTime.php">(Read More...)</a>
+            </p>
         </div>
     </section>
     <section class="contact">
